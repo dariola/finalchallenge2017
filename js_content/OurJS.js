0a77a4d80d4ab3fcc5014ade4bdbc53a1b1682da
Svend <svend.becker.25.03@gmail.com>
2017-12-07 11:39:52 +0100

    -added fade out

--- js_content/OurJS.js
+++ js_content/OurJS.js
@@ -1,80 +1,3 @@
-/*var audio = [];
-
-audio[0]=new Audio();
-audio[0].src="../sounds/office_sound.mp3";
-
-audio[1]= new Audio();
-audio[1].src="../sounds/ocean_ambient.mp3";
-
-audio[2]= new Audio();
-audio[3].src="../sounds/tibe_ambient.mp3" ;
-
-function playClip(){
-
-    if($('.item').eq(0).hasClass('active')){
-        audio[0].play();
-    }
-
-}
-
-
-
-function playClip() {
-
-
-    var sound1 = document.getElementById('ambient_sounds0');
-    var sound2 = document.getElementById('ambient_sounds1');
-    var sound3 = document.getElementById('ambient_sounds2');
-
-
-    var sounds_all = document.getElementsByTagName('audio');
-    for (var i = 0, len = audios.length; i < len; i++) {
-        sounds_all[i].pause();
-    }
-
-    var active =
-
-
-    switch (active){
-        case "1":
-            sound1.play();
-            break;
-        case "2":
-            sound2.play();
-            break;
-        case "3":
-            sound3.play();
-            break;
-        default:
-            sound3.play();
-    }
-
-}
-
-function playClip2(){
-
-    var sound1 = document.getElementById('ambient_sounds0');
-    var sound2 = document.getElementById('ambient_sounds1');
-    var sound3 = document.getElementById('ambient_sounds2');
-
-    if($('.item').eq(0).hasClass('active')){
-        sound1.play();
-    }else if ($('.item').eq(1).hasClass('active')){
-        sound2.play();
-    }
-    else
-        sound3.play();
-}
-
-
-$('.carousel-control').on("mouseover", function () {
-    var sound = document.getElementById('ambient_sounds0');
-    sound.play()
-});
-*/
-
-
-
 function playSound(whichone) {
 
     var sound1 = document.getElementById('ambient_sounds1');
@@ -83,12 +6,15 @@ function playSound(whichone) {
 
     switch (whichone){
         case 1:
+            sound1.volume=1;
             sound1.play();
             break;
         case 2:
+            sound2.volume=1;
             sound2.play();
             break;
         case 3:
+            sound3.volume=1;
             sound3.play();
             break;
         default:
@@ -105,17 +31,58 @@ function stopSound(whichone) {
 
     switch (whichone){
         case 1:
-            sound1.pause();
+            fadeOut(1);
             break;
         case 2:
-            sound2.pause();
+            fadeOut(2);
             break;
         case 3:
-            sound3.pause();
+            fadeOut(3);
             break;
         default:
-            sound3.pause();
+            stopAll();
     }
 
 }
 
+function fadeOut(whichone) {
+    var sound1 = document.getElementById('ambient_sounds1');
+    var sound2 = document.getElementById('ambient_sounds2');
+    var sound3 = document.getElementById('ambient_sounds3');
+
+    if(whichone===1) {
+        while (sound1.volume>0) {
+                sound1.volume -= 0.00005;
+        }
+        if(!sound1.volume>0){
+            sound1.pause();
+        }
+    }
+    else if(whichone ===2){
+        while (sound2.volume>0) {
+
+            sound2.volume -= 0.00005;
+        }
+        if(!sound2.volume>0){
+            sound2.pause();
+        }
+    }
+    else {
+        while (sound3.volume>0) {
+
+            sound3.volume -= 0.00005;
+        }
+        if(!sound3.volume>0){
+            sound3.pause();
+        }
+    }
+}
+
+function stopAll() {
+
+    var sounds_all = document.getElementsByTagName('audio');
+    for (var i = 0, len = audios.length; i < len; i++) {
+        sounds_all[i].pause();
+    }
+
+}
